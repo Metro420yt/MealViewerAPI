986debb465f21df879705358677f6e0fb18b6f20
Metro420yt <84623250+Metro420yt@users.noreply.github.com>
2022-03-08 19:24:10 -0500

    update 3/8

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 const fetch = require("node-fetch");
-const error = (msg) => {
-  throw new Error(msg);
-};
 
 /**
  * @param {string} school the school to return.
@@ -16,30 +13,29 @@ exports.get = async (school, date, options) => {
   const baseURL = "https://api.mealviewer.com/api/v4/school";
 
   //verifies  data
-  if (!school) error("School name must be provided!");
-  if (typeof school !== "string") error("Invalid school name!\n  Must be STRING");
+  if (!school) throw new Error("School name must be provided!");
+  if (typeof school !== "string") throw new Error("Invalid school name!\n  Must be STRING");
 
-  if (
-    date &&
-    (typeof date !== "string" ||
-      typeof date !== "number" ||
-      typeof date === "object")
-  )
-    error("Invalid date!\n  Must be STRING, NUMBER or OBJECT");
+  if (date && (
+    typeof date !== "string" &&
+    typeof date !== "number" &&
+    typeof date !== "object")
+  ) throw new Error("Invalid date!\n  Must be STRING, NUMBER or OBJECT");
 
   //formats date
   if (!date || date === null) date = Date.now();
-  date = new Date(date).toLocaleDateString().split("/").join("-");
-  if (typeof date === "string" || typeof date === "number")
+
+  if (typeof date !== "object") {
+    date = new Date(date).toLocaleDateString().split("/").join("-");
     date = `${date}/${date}`;
+  }
   else {
-    //not sure if this works yet, cant test it right now
     if (
-      typeof date.start !== "string" ||
-      typeof date.start !== "number" ||
-      typeof date.end !== "string" ||
+      typeof date.start !== "string" &&
+      typeof date.start !== "number" &&
+      typeof date.end !== "string" &&
       typeof date.end !== "number"
-    ) error("Invalid end/start date!\n  Must be a STRING, or NUMBER");
+    ) throw new Error("Invalid end/start date!\n  Must be a STRING, or NUMBER");
 
     if (!date.start) date.end = Date.now();
     if (!date.end) date.end = Date.now();
@@ -51,14 +47,15 @@ exports.get = async (school, date, options) => {
 
   //fetches data
   const url = `${baseURL}/${school}/${date}`;
-  const res = await (await fetch(url).catch(error)).json();
-  var respose = {};
+  const res = await (await fetch(url).catch(e => { throw new Error(e) })).json();
+  
+  const resposeArray = [];
 
   //gets menu data from results
   res.menuSchedules
-    .map((data) => data.menuBlocks)
-    .forEach((blocks) =>
-      blocks.forEach((block) => {
+    .forEach((blocks) => {
+      var menu = {}
+      blocks.menuBlocks.forEach((block) => {
         //gets items array
         const items = [];
         block.cafeteriaLineList.data[0].foodItemList.data
@@ -67,15 +64,19 @@ exports.get = async (school, date, options) => {
             if (!items.includes(item)) items.push(item);
           });
 
-        //adds data to respons var
-        respose[block.blockName.toLowerCase()] = items;
+        //adds items to menu
+        menu[block.blockName.toLowerCase()] = items;
       })
+      //adds date and menu
+      if (options?.date && Object.keys(menu).length !== 0) menu.date = blocks.dateInformation.dateFull
+      resposeArray.push(menu)
+    }
     );
 
   //adds any additional data
+  var respose = {items: resposeArray.filter(menu=>Object.keys(menu).length !== 0)};
   if (options?.rawData) respose.rawData = res;
   if (options?.url) respose.url = url;
-  if (options?.date) respose.date = date;
 
   return respose;
 };
